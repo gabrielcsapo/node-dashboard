f78989797ad0f2f069320c9040694a32625ab411
Gabriel Csapo <gabecsapo@gmail.com>
2016-06-20 00:56:16 -0700

    cleans up create graphs functions arranges dashboard in more of a grid to show more data at a glance

--- src/dashboard.js
+++ src/dashboard.js
@@ -25,22 +25,13 @@ window.createOSGraphs = function() {
             data.push(s[1]);
             labels.push(moment(s[0]).format('HH:mm:ss'));
         });
-        var color = chance.color({format: 'rgb'});
-        new Chart(document.querySelector('#memory-graph canvas'), {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: "Memory Usage",
-                    data: data,
-                    backgroundColor: color,
-                    borderColor: color.replace(')', ',0.1)')
-                }]
-            },
-            options: {
-                maintainAspectRatio: false
-            }
-        });
+        createGraph(
+            document.querySelector('#memory-graph canvas'),
+            labels,
+            data,
+            'Memory Usage',
+            'line'
+        );
     }());
 
     (function() {
@@ -51,196 +42,24 @@ window.createOSGraphs = function() {
             data.push(s[1]);
             labels.push(moment(s[0]).format('HH:mm:ss'));
         });
-        var color = chance.color({format: 'rgb'});
-        new Chart(document.querySelector('#cpu-graph canvas'), {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: "CPU Usage",
-                    data: data,
-                    backgroundColor: color,
-                    borderColor: color.replace(')', ',0.1)')
-                }]
-            },
-            options: {
-                maintainAspectRatio: false
-            }
-        });
+        createGraph(
+            document.querySelector('#cpu-graph canvas'),
+            labels,
+            data,
+            'CPU Usage',
+            'line'
+        );
     }());
 }
 
-var createUrlAverageTime = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-urlAverageTime-' + d.domain).checked = true;
-    Object.keys(d.urlAverageTime).forEach(function(u) {
-        labels.push(d.urlAverageTime[u].url);
-        data.push(d.urlAverageTime[u].time);
-    });
+var createGraph = function(ctx, labels, data, label, type) {
     var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-urlAverageTime canvas'), {
-        type: 'bar',
+    new Chart(ctx, {
+        type: type || 'bar',
         data: {
             labels: labels,
             datasets: [{
-                label: "UrlAverageTime",
-                data: data,
-                backgroundColor: color,
-                borderColor: color.replace(')', ',0.1)')
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
-}
-
-var createUrlGraph = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-urls-' + d.domain).checked = true;
-    Object.keys(d.urls).forEach(function(u) {
-        labels.push(d.urls[u].url);
-        data.push(d.urls[u].count);
-    });
-    var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-urls canvas'), {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "Url Hits",
-                data: data,
-                backgroundColor: color,
-                borderColor: color.replace(')', ',0.1)')
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
-}
-
-var createBrowserGraph = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-browsers-' + d.domain).checked = true;
-    Object.keys(d.browsers).forEach(function(s) {
-        labels.push(s);
-        data.push(d.browsers[s]);
-    });
-    var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-browsers canvas'), {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "Browsers",
-                data: data,
-                backgroundColor: color,
-                borderColor: color.replace(')', ',0.1)')
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
-}
-
-var createOSGraph = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-os-' + d.domain).checked = true;
-    Object.keys(d.os).forEach(function(s) {
-        labels.push(s);
-        data.push(d.os[s]);
-    });
-    var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-os canvas'), {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "OS",
-                data: data,
-                backgroundColor: color,
-                borderColor: color.replace(')', ',0.1)')
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
-}
-
-var createStatusGraph = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-status-' + d.domain).checked = true;
-    Object.keys(d.status).forEach(function(s) {
-        labels.push(s);
-        data.push(d.status[s]);
-    });
-    var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-status canvas'), {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "HTTP Statuses",
-                data: data,
-                backgroundColor: color,
-                borderColor: color.replace(')', ',0.1)')
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
-}
-
-var createReferrersGraph = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-referrers-' + d.domain).checked = true;
-    Object.keys(d.referrers).forEach(function(r) {
-        labels.push(r);
-        data.push(d.referrers[r]);
-    });
-    var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-referrers canvas'), {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "Refferers",
-                data: data,
-                backgroundColor: color,
-                borderColor: color.replace(')', ',0.1)')
-            }]
-        },
-        options: {
-            maintainAspectRatio: false
-        }
-    });
-}
-
-var createCountryGraph = function(d) {
-    var labels = [];
-    var data = [];
-    document.getElementById('label-graph-countries-' + d.domain).checked = true;
-    Object.keys(d.countries).forEach(function(c) {
-        labels.push(c);
-        data.push(d.countries[c]);
-    });
-    var color = chance.color({format: 'rgb'});
-    new Chart(document.querySelector('#' + d.domain + '-countries canvas'), {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: "Countries",
+                label: label,
                 data: data,
                 backgroundColor: color,
                 borderColor: color.replace(')', ',0.1)')
@@ -257,4 +76,116 @@ var createCountryGraph = function(d) {
             }
         }
     });
+}
+
+var createUrlAverageTime = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-urlAverageTime-' + d.domain).checked = true;
+    Object.keys(d.urlAverageTime).forEach(function(u) {
+        labels.push(d.urlAverageTime[u].url);
+        data.push(d.urlAverageTime[u].time);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-urlAverageTime canvas'),
+        labels,
+        data,
+        'Url Average Time'
+    );
+}
+
+var createUrlGraph = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-urls-' + d.domain).checked = true;
+    Object.keys(d.urls).forEach(function(u) {
+        labels.push(d.urls[u].url);
+        data.push(d.urls[u].count);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-urls canvas'),
+        labels,
+        data,
+        'Url Hits'
+    );
+}
+
+var createBrowserGraph = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-browsers-' + d.domain).checked = true;
+    Object.keys(d.browsers).forEach(function(s) {
+        labels.push(s);
+        data.push(d.browsers[s]);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-browsers canvas'),
+        labels,
+        data,
+        'Browsers'
+    );
+}
+
+var createOSGraph = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-os-' + d.domain).checked = true;
+    Object.keys(d.os).forEach(function(s) {
+        labels.push(s);
+        data.push(d.os[s]);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-os canvas'),
+        labels,
+        data,
+        'OS'
+    );
+}
+
+var createStatusGraph = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-status-' + d.domain).checked = true;
+    Object.keys(d.status).forEach(function(s) {
+        labels.push(s);
+        data.push(d.status[s]);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-status canvas'),
+        labels,
+        data,
+        'HTTP Statuses'
+    );
+}
+
+var createReferrersGraph = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-referrers-' + d.domain).checked = true;
+    Object.keys(d.referrers).forEach(function(r) {
+        labels.push(r);
+        data.push(d.referrers[r]);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-referrers canvas'),
+        labels,
+        data,
+        'Refferers'
+    );
+}
+
+var createCountryGraph = function(d) {
+    var labels = [];
+    var data = [];
+    document.getElementById('label-graph-countries-' + d.domain).checked = true;
+    Object.keys(d.countries).forEach(function(c) {
+        labels.push(c);
+        data.push(d.countries[c]);
+    });
+    createGraph(
+        document.querySelector('#' + d.domain + '-countries canvas'),
+        labels,
+        data,
+        'Countries'
+    );
 }
